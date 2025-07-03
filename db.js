b16624dc28b9611ae833a0cac8d1f92e856ddf8b
Aryan4548 <aryanyd6497@gmail.com>
2025-07-03 20:09:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false // 🛠️ Allow self-signed certs (safe for testing on Render)
   }
 });
 
